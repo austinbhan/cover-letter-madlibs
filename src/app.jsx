59cf290ec73183472a22f98c10e931f8449fda18
austinbhan <austinbhan@outlook.com>
2023-03-09 13:54:11 -0800

    usestate values created

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,21 @@
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-
 import '../src/app.css';
+import { useState } from 'react';
 
 export default function App() {
+  const [testValue, setTestValue] = useState('hi');
+
+  function noun() {
+    setTestValue('apple');
+  }
+
   return(
     <Router>
-      <h1>Hello World</h1>
+      <h1>Mad Lib App</h1>
+      <div>This is a trial sentence. Values go here: {testValue}</div>
+      <button onClick={noun}>Click me for a noun!</button>
     </Router>
   );
 }
