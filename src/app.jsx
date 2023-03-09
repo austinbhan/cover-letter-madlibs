72e615560ca1c1863798580aeb17db9f394131b3
austinbhan <austinbhan@outlook.com>
2023-03-09 15:47:16 -0800

    Text form submits correctly

--- src/app.jsx
+++ src/app.jsx
@@ -4,18 +4,43 @@ import {
 import '../src/app.css';
 import { useState } from 'react';
 
+
 export default function App() {
-  const [testValue, setTestValue] = useState('hi');
+  const [testValue, setTestValue] = useState('');
+  const [userText, setUserText] = useState('');
+  const [nounOne, setNounOne] = useState('');
 
   function noun() {
-    setTestValue('apple');
+    setTestValue('boob');
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setNounOne(`${userText}`);
+    setUserText('');
   }
 
   return(
     <Router>
       <h1>Mad Lib App</h1>
-      <div>This is a trial sentence. Values go here: {testValue}</div>
-      <button onClick={noun}>Click me for a noun!</button>
+      <div className="textForms">
+        <div>Noun is added here: {testValue}</div>
+        <div>{nounOne}</div>
+      </div>
+      <div className="nounButton">
+        <button onClick={noun}>Click me for a noun!</button>
+      </div>
+
+      <form className="textForm"
+        onSubmit={handleSubmit}>
+        <input 
+          name="userText"
+          value={userText} 
+          placeholder="User text"
+          onChange={e => setUserText(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+
     </Router>
   );
 }
