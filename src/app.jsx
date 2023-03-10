d9d0f898b8a7a3217de3a40e5c224494ba1f2029
austinbhan <austinbhan@outlook.com>
2023-03-10 15:05:36 -0800

    Reformatted forms

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,16 @@ import {
 } from 'react-router-dom';
 import '../src/app.css';
 import { useState } from 'react';
+import TextForm from './TextForm';
 
 
 export default function App() {
   const [userTextOne, setUserTextOne] = useState('');
   const [userTextTwo, setUserTextTwo] = useState('');
+  const [userTextThree, setUserTextThree] = useState('');
   const [nounOne, setNounOne] = useState('');
   const [nounTwo, setNounTwo] = useState('');
+  const [nounThree, setNounThree] = useState('');
 
   function handleSubmitOne(e) {
     e.preventDefault();
@@ -23,33 +26,38 @@ export default function App() {
     setUserTextTwo('');
   }
 
+  function handleSubmitThree(e) {
+    e.preventDefault();
+    setNounThree(`${userTextThree}`);
+    setUserTextThree('');
+  }
+
   return(
     <Router>
       <h1>Mad Lib App</h1>
       <div className="cover-letter">
-        <div>I am applying for the position of {nounOne} at {nounTwo}</div>
+        <div>I am applying for the position of {nounOne} at {nounTwo}. 
+        This is another noun: {nounThree}</div>
       </div>
 
-      <form className="textForm"
-        onSubmit={handleSubmitOne}>
-        <input 
-          name="userText"
-          value={userTextOne} 
-          placeholder="Noun 1"
-          onChange={e => setUserTextOne(e.target.value)} />
-        <button type="submit">Submit Noun</button>
-      </form>
+      <TextForm 
+        handleSubmit={handleSubmitOne}
+        userText={userTextOne}
+        setUserText={setUserTextOne}
+        term={'Noun 1'}/>
 
+      <TextForm 
+        handleSubmit={handleSubmitTwo}
+        userText={userTextTwo}
+        setUserText={setUserTextTwo}
+        term={'Noun 2'}/>
 
-      <form className="textForm"
-        onSubmit={handleSubmitTwo}>
-        <input 
-          name="userText"
-          value={userTextTwo} 
-          placeholder="Noun 1"
-          onChange={e => setUserTextTwo(e.target.value)} />
-        <button type="submit">Submit Noun</button>
-      </form>
+      <TextForm 
+        handleSubmit={handleSubmitThree}
+        userText={userTextThree}
+        setUserText={setUserTextThree}
+        term={'Noun 3'}/>
+
     </Router>
   );
 }
